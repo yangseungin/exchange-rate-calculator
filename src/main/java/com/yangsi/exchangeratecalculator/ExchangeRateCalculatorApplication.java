26d37d71ab732e10c943c1ab4857e9201882dd8e
seungin yang <rhfpdk92@naver.com>
2022-05-25 00:35:34 +0900

    Initial commit

---
+++ src/main/java/com/yangsi/exchangeratecalculator/ExchangeRateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.yangsi.exchangeratecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangsi/exchangeratecalculator/ExchangeRateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangsi.exchangeratecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
