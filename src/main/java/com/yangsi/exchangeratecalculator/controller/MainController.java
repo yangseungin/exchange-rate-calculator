eeb24333f620df77ae5986c8c273a190edc2c91f
seungin yang <rhfpdk92@naver.com>
2022-06-12 23:33:59 +0900

    환전 화면 추가

---
+++ src/main/java/com/yangsi/exchangeratecalculator/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.yangsi.exchangeratecalculator.controller;
+
+import com.yangsi.exchangeratecalculator.domain.CountryEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("remittanceCountry", CountryEnum.remittanceCountry());
+        model.addAttribute("recipientCountries", CountryEnum.recipientCountries());
+
+        return "index";
+    }
+}
