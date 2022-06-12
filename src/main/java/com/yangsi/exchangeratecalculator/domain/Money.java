3213325c4f7517f4a91bd761f416ed40b9dda129
seungin yang <rhfpdk92@naver.com>
2022-06-13 01:48:19 +0900

    생성자 범위제한 및 validate 추가

--- src/main/java/com/yangsi/exchangeratecalculator/domain/Money.java
+++ src/main/java/com/yangsi/exchangeratecalculator/domain/Money.java
@@ -1,18 +1,22 @@
 package com.yangsi.exchangeratecalculator.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Money {
 
     private final BigDecimal value;
     private final CountryEnum country;
 
     public static Money of(double amount, CountryEnum country) {
+        if(amount<=0){
+            throw new IllegalArgumentException("유효하지않은 돈입니다.");
+        }
         return new Money(BigDecimal.valueOf(amount), country);
     }
 
